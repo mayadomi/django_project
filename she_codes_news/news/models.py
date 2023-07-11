266907c5f30e8c701b13db2ad9db7b8cc42b117e
Maya Dominice <maya.dominice@gmail.com>
2023-07-11 20:34:16 +1000

    added image url to model and form and set template to show new url.

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -6,3 +6,5 @@ class NewsStory(models.Model):
     author = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     content = models.TextField()
+    image_url = models.URLField(null=True)
+
