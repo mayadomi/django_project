eae2aaa7405aee6b8adf7166769be053808794dc
Maya Dominice <maya.dominice@gmail.com>
2023-07-14 13:39:59 +1000

    Addition of an account view page - not sure if this is the best way to do it... but tis a start.

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreateAccountView
+from .views import CreateAccountView, ViewAccount
 
 app_name = 'users'
 
 urlpatterns = [
      path('create-account/', CreateAccountView.as_view(), name='createAccount'),
+     path('view-account/', ViewAccount.as_view(), name =  'viewAccount'),
 ]
--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -6,9 +6,14 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.views import generic
 from .models import CustomUser
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 class CreateAccountView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/createAccount.html'
+
+class ViewAccount(generic.CreateView):
+    form_class = CustomUserChangeForm
+    template_name = 'users/viewAccount.html'
+    success_url = reverse_lazy('view-account')
